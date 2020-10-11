b7202bc3518690832867ee6d74201da4534f68a7
dolphinlin1211ok <72702900+dolphinlin1211ok@users.noreply.github.com>
2020-10-12 00:38:06 +0800

    upload files

---
+++ script.js
@@ -0,0 +1,10 @@
+const demo = document.getElementById("demo");
+const title = demo.innerHTML;
+const charArray = title.split("");
+let animationHTML = "";
+charArray.forEach((char, index) => {
+  animationHTML =
+    animationHTML +
+    `<div class="char" style="animation-delay:${index * 2}s">${char}</div>`;
+});
+demo.innerHTML = animationHTML;
